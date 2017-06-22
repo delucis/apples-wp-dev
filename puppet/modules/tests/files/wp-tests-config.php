3658338d76ae847d61607fc457b78e9440424aa5
delucis <swithinbank@gmail.com>
2017-06-22 03:01:40 +0200

    Rename vagrantpress.dev → claraiannotta.dev.

--- puppet/modules/tests/files/wp-tests-config.php
+++ puppet/modules/tests/files/wp-tests-config.php
@@ -33,8 +33,8 @@ define( 'DB_COLLATE', '' );
 
 $table_prefix  = 'wptests_';   // Only numbers, letters, and underscores please!
 
-define( 'WP_TESTS_DOMAIN', 'vagrantpress.dev' );
-define( 'WP_TESTS_EMAIL', 'nobody@vagrantpress.dev' );
+define( 'WP_TESTS_DOMAIN', 'claraiannotta.dev' );
+define( 'WP_TESTS_EMAIL', 'nobody@claraiannotta.dev' );
 define( 'WP_TESTS_TITLE', 'WordPress automated tests' );
 
 define( 'WP_PHP_BINARY', 'php' );
